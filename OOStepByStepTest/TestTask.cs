bd4ac80dc9fb2f42f0725a7aeb38c64e49e4aa92
Xueqian Li <XLi112@slb.com>
2023-11-02 16:54:50 +0800

    test: Should_return_name_and_age_when_self_introduce_given_a_student/teacher

--- OOStepByStepTest/TestTask.cs
+++ OOStepByStepTest/TestTask.cs
@@ -12,5 +12,21 @@ namespace OOStepByStepTest
             var res = p.SelfIntroduce();
             Assert.Equal("My name is Tom, I am 21 years old.", res);
         }
+
+        [Fact]
+        public void Should_return_name_and_age_when_self_introduce_given_a_student()
+        {
+            var p = new Student("Tom", 18);
+            var res = p.SelfIntroduce();
+            Assert.Equal("My name is Tom, I am 18 years old. I am a student.", res);
+        }
+
+        [Fact]
+        public void Should_return_name_and_age_when_self_introduce_given_a_teacher()
+        {
+            var p = new Teacher("Amy", 30);
+            var res = p.SelfIntroduce();
+            Assert.Equal("My name is Amy, I am 30 years old. I am a teacher.", res);
+        }
     }
 }
