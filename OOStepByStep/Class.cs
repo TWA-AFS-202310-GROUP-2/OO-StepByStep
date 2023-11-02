521af124cc07652a530cb62bf36713c6b668b1bd
Tju-Bibibo <34788685+Tju-Bibibo@users.noreply.github.com>
2023-11-02 20:35:12 +0800

    feat: add class add student function and ac4 test

--- OOStepByStep/Class.cs
+++ OOStepByStep/Class.cs
@@ -1,8 +1,30 @@
 namespace OOStepByStep;
 using System.Collections.Generic;
+using System.Text;
 public class Class
 {
     public Teacher Teacher { get; set; }
     public List<Student> Students { get; set; } = new List<Student>();
     public int ClassNumber { get; set; }
+
+    public string AddStudent(Student student)
+    {
+        Students.Add(student);
+
+        student.ClassNumber = ClassNumber;
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine($"{Teacher.Introduce()} Welcome {student.Name} join class {ClassNumber}.");
+
+        foreach (var stud in Students)
+        {
+            if (stud.Name != student.Name)
+            {
+                sb.AppendLine($"{stud.Introduce()} Welcome {student.Name} join class {ClassNumber}.");
+            }
+        }
+
+        return sb.ToString();
+    }
 }
--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -61,5 +61,29 @@ namespace OOStepByStepTest
 
             Assert.Equal("My name is Amy. I am 30 years old. I am a teacher of class 2.", result);
         }
+
+        [Fact]
+        public void Test_AddStudent_To_Class()
+        {
+            var teacher = new Teacher("Amy", 30, 2);
+
+            var class2 = new Class { ClassNumber = 2, Teacher = teacher };
+
+            var studentTom = new Student("Tom", 18, 2);
+
+            var studentJim = new Student("Jim", 18, 2);
+
+            class2.AddStudent(studentTom);
+
+            var result = class2.AddStudent(studentJim);
+
+            var expectedOutput =
+
+                "My name is Amy. I am 30 years old. I am a teacher of class 2. Welcome Jim join class 2.\r\n" +
+
+                "My name is Tom. I am 18 years old. I am a student of class 2. Welcome Jim join class 2.\r\n";
+
+            Assert.Equal(expectedOutput, result);
+        }
     }
 }
