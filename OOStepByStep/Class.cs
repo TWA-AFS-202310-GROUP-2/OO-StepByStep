4d92972b4d2e2d65ffcc5dad951a5a1b21a33d4e
Xueqian Li <XLi112@slb.com>
2023-11-02 18:14:16 +0800

    feat: students and teachers welcome new comer in the class function implement

---
+++ OOStepByStep/Class.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOStepByStep
+{
+    public class Class
+    {
+        public Class(int classNumber)
+        {
+            ClassNumber = classNumber;
+        }
+
+        public Class(int classNumber, Teacher teacher)
+        {
+            Teacher = teacher;
+            teacher.ClassNumber = classNumber;
+            ClassNumber = classNumber;
+        }
+
+        public Teacher Teacher { get; set; }
+
+        public List<Student> Students { get; set; } = new List<Student>();
+
+        public int ClassNumber { get; set; }
+
+        public string JoinStudent(Student student)
+        {
+            Students.Add(student);
+            student.ClassNumber = ClassNumber;
+            StringBuilder sb = new StringBuilder();
+
+            if (Teacher != null)
+            {
+                sb.AppendLine($"{Teacher?.SelfIntroduce()} Welcome {student.Name} join class {ClassNumber}.");
+            }
+
+            for (int i = 0; i < Students.Count - 1; i++)
+            {
+                sb.AppendLine($"{Students[i]?.SelfIntroduce()} Welcome {student.Name} join class {ClassNumber}.");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
