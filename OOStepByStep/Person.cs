16c9a62077c401d42dd553339ec3c57df1bee0ba
Zliu53 <ZLiu53@slb.com>
2023-11-02 16:40:59 +0800

    Feat: create person class so it can introduce it self

---
+++ OOStepByStep/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace OOStepByStep;
+
+public class Person
+{
+    public string name;
+    public int age;
+
+    Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public virtual string Speak()
+    {
+        return $"My name is {name}, I am {age} years old.";
+    }
+
+}
