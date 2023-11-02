ea645fdf951c70b0fd2e90d02d8d6b2bc53c0f92
Shang Huang <shuang19@slb.com>
2023-11-02 17:22:11 +0800

    feat: show a message when a person do self-introduction given name and age

---
+++ OOStepByStep/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOStepByStep
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        // AC1: A person can introduce him/herself with name and age like: "My name is Tom. I am 21 years old."
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string SelfIntro()
+        {
+            return $"My name is {this.name}. I am {this.age} years old.";
+        }
+    }
+}
---
+++ OOStepByStepTest/PersonTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using OOStepByStep;
+
+namespace OOStepByStepTest
+{
+    public class PersonTest
+    {
+        [Theory]
+        [InlineData("Tom", 21)]
+        public void Should_show_msg_when_person_self_introduce_given_name_age(string name, int age)
+        {
+            Person person = new Person(name, age);
+
+            string msg = person.SelfIntro();
+
+            Assert.Equal($"My name is Tom. I am 21 years old.", msg);
+        }
+    }
+}
