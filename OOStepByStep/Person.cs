1c86a585c3aefad8ac4fc0236afae28de334f088
Xueqian Li <XLi112@slb.com>
2023-11-02 16:38:30 +0800

    feat: create person class that can introduce himself/herself

---
+++ OOStepByStep/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace OOStepByStep
+{
+    using System;
+    public class Person
+    {
+        public Person()
+        {
+        }
+
+        public Person(string name, int age)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public string SelfIntroduce()
+        {
+            return $"My name is {Name}, I am {Age} years old.";
+        }
+    }
+}
