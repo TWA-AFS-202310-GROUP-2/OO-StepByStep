9d73486c4dddb6eb205cf60894786a24e186032c
Zliu53 <ZLiu53@slb.com>
2023-11-02 17:21:58 +0800

    Feat&Test: create student and teacher class so it can introduce them self with job

---
+++ OOStepByStep/Teacher.cs
@@ -0,0 +1,13 @@
+﻿namespace OOStepByStep;
+
+public class Teacher : Person
+{
+    public Teacher(string name, int age) : base(name, age)
+    {
+    }
+
+    public override string Speak()
+    {
+        return base.Speak()+" I am a teacher.";
+    }
+}
--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -16,5 +16,29 @@ namespace OOStepByStepTest
 
             Assert.Equal($"My name is {name}, I am {age} years old.",introduction);
         }
+
+        [Fact]
+        public void Should_return_student_introduction_when_student_given_introduction()
+        {
+            string name = "Tom";
+            int age = 14;
+
+            var student = new Student(name, age);
+            string introduction = student.Speak();
+
+            Assert.Equal($"My name is {name}, I am {age} years old. I am a student.", introduction);
+        }
+
+        [Fact]
+        public void Should_return_teacher_introduction_when_teacher_given_introduction()
+        {
+            string name = "Jenny";
+            int age = 32;
+
+            var teacher = new Teacher(name, age);
+            string introduction = teacher.Speak();
+
+            Assert.Equal($"My name is {name}, I am {age} years old. I am a teacher.", introduction);
+        }
     }
 }
